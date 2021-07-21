03157897ea9fb4a239fabaab4ce86f91a738b847
NatalyaMamicheva <69267434+NatalyaMamicheva@users.noreply.github.com>
2021-07-21 23:31:01 +0400

    Update settings.py

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -165,7 +165,7 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, "static"),
 #)
-STATICFILES_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
